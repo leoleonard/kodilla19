0c3092866675cd6adab18199851c94f5290e9911
Damian Juzyk <damian.juzyk@gmail.com>
2018-02-09 09:10:13 +0100

    add 19.4

---
+++ reducer.js
@@ -0,0 +1,70 @@
+import {ADD_COMMENT, EDIT_COMMENT, REMOVE_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions'
+
+const initialState = {
+  comments: [],
+  users: []
+}
+
+// function reducer(state, action) {
+//   if (!state) {
+//     return initialState;
+//   }
+//   return state;
+// }
+
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        case ADD_COMMENT:
+            return Object.assign({}, state, {
+                comments: [
+                {
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                }
+                , ...state]
+            })
+        default:
+            return state;
+
+        case REMOVE_COMMENT:
+          return Object.assign({}, state, {
+            comments: state.comments.filter(comment => comment.id !== action.id)
+          });
+          default:
+            return state;
+
+        case EDIT_COMMENT:
+          return Object.assign({}, state, {
+            comments: [
+              {
+                id: action.id,
+                text: action.text
+              }
+              , ...state]
+          })
+          default:
+              return state;
+
+          case THUMB_UP_COMMENT:
+            return Object.assign({}, state, {
+              comments: [
+                id: action.id,
+                votes: votes++
+                , ...state ]
+            })
+            default
+              return state;
+
+              case THUMB_DOWN_COMMENT:
+                return Object.assign({}, state, {
+                  comments: [
+                    id: action.id,
+                    votes: votes--
+                    , ...state ]
+                })
+                default
+                  return state;
+
+            }
+          }
